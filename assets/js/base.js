620a3c7ecae92445652ac6ed8b350ee09d924745
janbean <janbean@163.com>
2015-01-04 01:36:45 +0800

    header anim and one-word

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,3 +1,69 @@
+jQuery.fn.rotate = function(angle,whence) { 
+    var p = this.get(0); 
+ 
+    // we store the angle inside the image tag for persistence  
+    if (!whence) { 
+        p.angle = ((p.angle==undefined?0:p.angle) + angle) % 360; 
+    } else { 
+        p.angle = angle; 
+    } 
+ 
+    if (p.angle >= 0) { 
+        var rotation = Math.PI * p.angle / 180; 
+    } else { 
+        var rotation = Math.PI * (360+p.angle) / 180; 
+    } 
+    var costheta = Math.round(Math.cos(rotation) * 1000) / 1000; 
+    var sintheta = Math.round(Math.sin(rotation) * 1000) / 1000; 
+    //alert(costheta+","+sintheta);  
+  
+    if (document.all && !window.opera) { 
+        var canvas = document.createElement('img'); 
+ 
+        canvas.src = p.src; 
+        canvas.height = p.height; 
+        canvas.width = p.width; 
+ 
+        canvas.style.filter = "progid:DXImageTransform.Microsoft.Matrix(M11="+costheta+",M12="+(-sintheta)+",M21="+sintheta+",M22="+costheta+",SizingMethod='auto expand')"; 
+    } else { 
+        var canvas = document.createElement('canvas'); 
+        if (!p.oImage) { 
+            canvas.oImage = new Image(); 
+            canvas.oImage.src = p.src; 
+        } else { 
+            canvas.oImage = p.oImage; 
+        } 
+ 
+        canvas.style.width = canvas.width = Math.abs(costheta*canvas.oImage.width) + Math.abs(sintheta*canvas.oImage.height); 
+        canvas.style.height = canvas.height = Math.abs(costheta*canvas.oImage.height) + Math.abs(sintheta*canvas.oImage.width); 
+ 
+        var context = canvas.getContext('2d'); 
+        context.save(); 
+        if (rotation <= Math.PI/2) { 
+            context.translate(sintheta*canvas.oImage.height,0); 
+        } else if (rotation <= Math.PI) { 
+            context.translate(canvas.width,-costheta*canvas.oImage.height); 
+        } else if (rotation <= 1.5*Math.PI) { 
+            context.translate(-costheta*canvas.oImage.width,canvas.height); 
+        } else { 
+            context.translate(0,-sintheta*canvas.oImage.width); 
+        } 
+        context.rotate(rotation); 
+        context.drawImage(canvas.oImage, 0, 0, canvas.oImage.width, canvas.oImage.height); 
+        context.restore(); 
+    } 
+    canvas.id = p.id; 
+    canvas.angle = p.angle; 
+    p.parentNode.replaceChild(canvas, p); 
+} 
+ 
+jQuery.fn.rotateRight = function(angle) { 
+    this.rotate(angle==undefined?90:angle); 
+} 
+ 
+jQuery.fn.rotateLeft = function(angle) { 
+    this.rotate(angle==undefined?-90:-angle); 
+} 
 $(document).ready(function() {
 function getByClass(oParent, sClass) {
         var aEle=oParent.getElementsByTagName('*');
@@ -113,4 +179,52 @@ window.onscroll = function () {
 	setOpacity();
 };
 setOpacity();
+	var rotate = 0;
+	var clickTime = 0;
+	var toRota = 90;
+	/*设置点击后一言变换*/
+	$(".one-word-btn").click(function(event){
+		var animItem = $("#one-word-btn");
+		if(clickTime==0){
+			clickTime = 1;
+			rotate = Number(rotate) + toRota;
+		} else {
+			clickTime = 0;
+			rotate = Number(rotate) - toRota;
+		}
+
+		animItem.css("-webkit-transform","scale(1.5,1.5) rotate("+rotate+"deg)");
+		animItem.css("-moz-transform","scale(1.5,1.5) rotate("+rotate+"deg)");
+		animItem.css("transform","scale(1.5,1.5) rotate("+rotate+"deg)");
+
+		setTimeout(function(){
+			if(clickTime==0){
+				// clickTime = 1;
+				rotate = Number(rotate) + toRota;
+			} else {
+				rotate = Number(rotate) - toRota;
+			}
+			animItem.removeAttr("style");
+			// animItem.removeClass("-webkit-transform");
+			// animItem.removeClass("-moz-transform");
+			// animItem.removeClass("transform");
+			// animItem.css("-webkit-transform","scale(1,1) rotate("+rotate+"deg)");
+			// animItem.css("-moz-transform","scale(1,1) rotate("+rotate+"deg)");
+			// animItem.css("transform","scale(1,1) rotate("+rotate+"deg)");
+		},200),
+
+		console.log(event);
+		// console.log('click');
+		//调用jQuery脚本请求
+		// jQuery.getScript('http://api.hitokoto.us/rand?uid=3153',function(data,err){  
+		// 		console.log("ajax 一言");
+		// 		console.log(err);
+		// 	});
+		// });
+		$.ajax({
+		  url: 'http://api.hitokoto.us/rand?uid=3153&encode=jsc&fun=async',
+		  dataType: "jsonp"
+		});
+	});
+
 });
